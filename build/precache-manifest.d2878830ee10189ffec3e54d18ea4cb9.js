26ff1afe4ecf0e46e3cb2115f14dd6895fca94d1
Kamil Qadeer <kamilqadeer13@gmail.com>
2021-07-04 22:43:49 +0500

    first commit

---
+++ build/precache-manifest.d2878830ee10189ffec3e54d18ea4cb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc52b915712a2f6221b023d4e60a68ca",
+    "url": "/KamilQadeer/todo_react.git/index.html"
+  },
+  {
+    "revision": "678418306870757d98cb",
+    "url": "/KamilQadeer/todo_react.git/static/css/main.855a11a9.chunk.css"
+  },
+  {
+    "revision": "453ccc6754c5abb64400",
+    "url": "/KamilQadeer/todo_react.git/static/js/2.326094fb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/KamilQadeer/todo_react.git/static/js/2.326094fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "678418306870757d98cb",
+    "url": "/KamilQadeer/todo_react.git/static/js/main.26c271c4.chunk.js"
+  },
+  {
+    "revision": "2e9729146ecb8bf1be86",
+    "url": "/KamilQadeer/todo_react.git/static/js/runtime-main.87af6563.js"
+  }
+]);
